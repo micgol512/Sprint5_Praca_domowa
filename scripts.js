292b3e74ee818977200ed09e9eba5b39b4edb746
Michał Gołębiowski <m.golebiowski.fm@gmail.com>
2024-07-17 21:22:34 +0200

    After Feedback

--- scripts.js
+++ scripts.js
@@ -27,63 +27,74 @@ const personsIn = [
 ];
 // ZADANIE 1
 function makeNickname(persons) {
-  persons.forEach((person) => {
-    let nick = "";
+  return persons.map((person) => {
     if (
       typeof person.firstName === "string" &&
       typeof person.lastName === "string" &&
       person.firstName.length >= 3 &&
       person.lastName.length >= 3
     ) {
-      nick =
-        cuttingLetters(null, -3, person.firstName) +
-        cuttingLetters(0, 3, person.lastName);
-      nick = nick[0].toUpperCase() + nick.slice(1);
-      person.nickname = nick;
+      const preNick = person.firstName
+        .slice(-3)
+        .toLowerCase()
+        .split("")
+        .reverse()
+        .join("");
+      const afterNick = person.lastName
+        .slice(0, 3)
+        .toLowerCase()
+        .split("")
+        .reverse()
+        .join("");
+      const nick = `${preNick}${afterNick}`;
+      return {
+        ...person,
+        nickname: `${nick[0].toUpperCase()}${nick.slice(1)}`,
+      };
+    } else {
+      return {
+        ...person,
+      };
     }
   });
 }
-function cuttingLetters(start, end, string) {
-  if (end < 0) {
-    return string.slice(end).toLowerCase().split("").reverse().join("");
-  } else {
-    return string.slice(start, end).toLowerCase().split("").reverse().join("");
-  }
-}
-makeNickname(personsIn);
-console.log("Zadanie 1: ", personsIn);
+
+const personsEx1 = makeNickname(personsIn);
+console.log("Zadanie 1: ", personsEx1);
 
 // Zadanie 2
 function personsWithNickname(persons) {
-  const nickPersons = persons.filter(
-    (person) => typeof person.nickname === "string"
-  );
-  nickPersons.forEach(({ firstName, lastName }, indeks) => {
-    let age = firstName.length + lastName.length;
-    if (age % 2 !== 0) {
-      age = Math.ceil(
-        Object.keys(nickPersons[indeks]).reduce(
-          (sum, key) => (sum += key.length),
-          0
-        ) / (indeks === 0 ? 1 : index)
-      );
-    }
-    nickPersons[indeks].age = age;
-  });
-  return nickPersons;
+  return persons
+    .filter((person) => person.nickname)
+    .map((person, index) => {
+      let age = person.firstName.length + person.lastName.length;
+      if (age % 2 === 1) {
+        age = Math.ceil(
+          Object.keys(person).reduce((sum, key) => (sum += key.length), 0) /
+            (index === 0 ? 1 : index)
+        );
+      }
+      return {
+        ...person,
+        age,
+      };
+    });
 }
-const personsWithAge = personsWithNickname(personsIn);
+const personsWithAge = personsWithNickname(personsEx1);
 console.log("Zadanie 2:", personsWithAge);
 
 // Zadanie 3
 function mostPopularChar(word) {
-  const wordTab = word.toLowerCase().split("");
-  let counts = {};
-  wordTab.forEach((value) => {
-    //Tu podziękowania za ostatniego live z pytaniem o sprawdzanie czy liczba "undefined" wiele mi tu uprościło :)
-    counts[value] = (counts[value] || 0) + 1;
-  });
-  counts = Object.entries(counts).reduce((max, curr) => {
+  const wordTab = word
+    .toLowerCase()
+    .split("")
+    .map((value) => ({ [value]: 1 }))
+    .reduce((acc, curr) => {
+      const key = Object.keys(curr)[0];
+      acc[key] = (acc[key] || 0) + curr[key];
+      return acc;
+    }, {});
+  return Object.entries(wordTab).reduce((max, curr) => {
     if (curr[1] > max[1]) {
       return curr;
     } else if (curr[1] < max[1]) {
@@ -96,12 +107,11 @@ function mostPopularChar(word) {
       }
     }
   });
-  return counts;
 }
 function analyzePersons(persons) {
   persons.forEach((element) => {
     const temp = mostPopularChar(
-      element.firstName + element.lastName + element.nickname
+      `${element.firstName}${element.lastName}${element.nickname}`
     );
     element.mostCommonLetter = { letter: temp[0], count: temp[1] };
   });
